4759f2b7955f89c111b978a913c1d27004662dc3
LongZhou <pancnlz@hotmail.com>
2013-05-01 18:22:45 +0800

    add page switch

--- Public/js/md/rander.js
+++ Public/js/md/rander.js
@@ -1,26 +1,41 @@
 define(function(require, exports, module) {
     var $ = require('jquery');
 
+    var sildeContain = function(tpl) {
+        var $opage = $('#page');
+
+        if ($opage.length > 0) {
+            $opage.fadeOut(100, function() {
+                $(this).remove();
+                $('#main').append(tpl);
+                $('#page').addClass('page_show');
+            });
+        } else {
+            $('#main').append(tpl);
+            $('#page').addClass('page_show');
+        }
+    };
+
     var randerPage = {
         '#index': function() {
             var tpl = require.async('/tpl/index.html', function(tpl) {
-                $('#main').append(tpl);
+                sildeContain(tpl);
             });
         },
-        '#video': function() {
-            var tpl = require.async('/tpl/video.html', function(tpl) {
-                $('#main').append(tpl);
+        '#files': function() {
+            var tpl = require.async('/tpl/files.html', function(tpl) {
+                sildeContain(tpl);
+            });
+        },
+        '#settings': function() {
+            var tpl = require.async('/tpl/settings.html', function(tpl) {
+                sildeContain(tpl);
             });
         }
     };
 
-    var clearRander = function() {
-        $('#page').remove();
-    };
-
     exports.paint = function(hash) {
         if (randerPage[hash] !== undefined) {
-            clearRander();
             randerPage[hash]();
         } else { // not exist, point to index
             location.hash = '#index';
